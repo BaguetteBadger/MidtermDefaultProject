bbfd66fb6a04b42d32e9c1292dea2291209c5dc0
BaguetteBadger <tangohoneybadger@gmail.com>
2026-03-08 10:57:42 -0500

    Cleaning + adding library class for commands
    Cleaned up some code in the class that holds the main function as well as added a new class called library to hold all commands relating to the user's inputs

---
+++ MidtermDefaultProject/Library.cs
@@ -0,0 +1,27 @@
+﻿
+namespace MidtermDefaultProject
+{
+    public class Library
+    {
+
+        public static void PrintCatalogue(Book[] library)
+        {
+            for(int i = 0; i < library.Length; i++)
+            {
+                Console.WriteLine(library[i]);
+            }
+        }
+
+        public static void PrintAvaliableCatalogue(Book[] library)
+        {
+            for (int i = 0; i < library.Length; i++)
+            {
+                if (!library[i].IsCheckedOut)
+                {
+                    Console.WriteLine(library[i]);
+                }
+            }
+        }
+
+    }
+}
--- MidtermDefaultProject/MidtermDefaultProject.cs
+++ MidtermDefaultProject/MidtermDefaultProject.cs
@@ -5,44 +5,63 @@
         public static void Main()
         {
             Book[] books = SplitBookData("Library.csv");
-            /*Book book1 = new Book("sad", "asd", 300, 2000, "124", true);
 
-            Console.WriteLine(book1);*/
+            Library.PrintCatalogue(books);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Library.PrintAvaliableCatalogue(books);
+  
+
+
+
+
+
         }
-        static bool IsBool(string yn){
-            if(yn=="yes"){
+
+        static bool IsBool(string yn)
+        {
+            if (yn == "Yes")
+            {
                 return true;
             }
             return false;
         }
-        static Book[] SplitBookData(string path){
+
+        private static Book[] SplitBookData(string path)
+        {
             using StreamReader reader = new StreamReader(path);
-reader.ReadLine();
-Book[] books = new Book[GetLineCount(path)-1];
-for (int i = 0; i < GetLineCount(path)-1; i++) {
-string line = reader.ReadLine();
-string[] bookVariables = line.Split(',');
-books[i] = new Book(bookVariables[0], bookVariables[1], int.Parse(bookVariables[2]), int.Parse(bookVariables[3]), bookVariables[4], IsBool(bookVariables[5]));
+            reader.ReadLine();
+            Book[] books = new Book[GetLineCount(path) - 1];
+            for (int i = 0; i < GetLineCount(path) - 1; i++)
+            {
+                string line = reader.ReadLine();
+                string[] bookVariables = line.Split(',');
+                books[i] = new Book(bookVariables[0], bookVariables[1], int.Parse(bookVariables[2]), int.Parse(bookVariables[3]), bookVariables[4], IsBool(bookVariables[5]));
+
+            }
+
+            return books;
+        }
+
+        private static int GetLineCount(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new Exception("file not found");
+            }
+            int count = 0;
+            using StreamReader reader = new StreamReader(path);
+            while (!reader.EndOfStream)
+            {
+                reader.ReadLine();
+                count++;
+            }
+            return count;
+        }
 
     }
-            static int GetLineCount(string path)
-{
-    if (!File.Exists(path))
-    {
-        throw new Exception("file not found");
-    }
-    int count = 0;
-    using StreamReader reader = new StreamReader(path);
-    while (!reader.EndOfStream)
-    {
-        reader.ReadLine();
-        count++;
-    }
-    return count;
 }
-            return books;
-}
-    }
- }
         
  
