2be275f27f66cfc4edb97a04149cf4d1f0ff1768
maiLbac <liam.l.morrissey@gmail.com>
2026-03-07 23:19:11 -0600

    Update MidtermDefaultProject.cs

--- MidtermDefaultProject/MidtermDefaultProject.cs
+++ MidtermDefaultProject/MidtermDefaultProject.cs
@@ -4,9 +4,16 @@
     {
         public static void Main()
         {
-            Book book1 = new Book("sad", "asd", 300, 2000, "124", true);
+            Book[] books= SplitBookData("Library.csv")
+            /*Book book1 = new Book("sad", "asd", 300, 2000, "124", true);
 
-            Console.WriteLine(book1);
+            Console.WriteLine(book1);*/
+        }
+        static bool IsBool(string yn){
+            if(yn=="yes"){
+                return true;
+            }
+            return false;
         }
         static Book[] SplitBookData(string path){
             using StreamReader reader = new StreamReader(path);
@@ -15,7 +22,7 @@ Book[] books = new Book[GetLineCount(path)-1];
 for (int i = 0; i < GetLineCount(path)-1; i++) {
 string line = reader.ReadLine();
 string[] bookVariables = line.Split(',');
-movies[i] = new Book(bookVariables[0], int.Parse(bookVariables[3]), bookVariables[2], bookVariables[1], double.Parse(bookVariables[4]));
+books[i] = new Book(bookVariables[0], bookVariables[1], int.Parse(bookVariables[2]), int.Parse(bookVariables[3]), bookVariables[4], IsBool(bookVariables[5]));
 
     }
             static int GetLineCount(string path)
